0a1b8a016c54a7dbf952275735bb2ed445eaa06d
Long JiaWei <1580550924@qq.com>
2026-06-02 20:09:41 +0800

    ex1

---
+++ ex1.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+
+int main()
+{
+	int x {};
+	std::cout << "Enter an integer: ";
+	std::cin >> x;
+	std::cout << "Double that number is: " << x * 2 << std::endl;
+	return 0;
+}
