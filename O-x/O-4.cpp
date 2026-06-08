ea16e9e3b3074d819b2e20733c54f3b2fca51d7c
Long JiaWei <1580550924@qq.com>
2026-06-08 10:40:41 +0800

    o-4

---
+++ O-x/O-4.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+
+void printBit(int x, int pow)
+{
+	std::cout << ((x / pow) % 2);
+}
+
+void print8Bit(int x)
+{
+	printBit(x, 128);
+	printBit(x, 64);
+	printBit(x, 32);
+	printBit(x, 16);
+	std::cout << ' ';
+	printBit(x, 8);
+	printBit(x, 4);
+	printBit(x, 2);
+	printBit(x, 1);
+}
+
+int main()
+{
+	std::cout << "Enter an integer between 0 and 255: ";
+	int x;
+	std::cin >> x;
+
+	print8Bit(x);
+
+	std::cout << '\n';
+}
